f65c30b7fde35f05a348f07132a6c9c9f8b9a9b4
IgorIF <igarif1992@gmail.com>
2021-08-21 03:21:24 +0300

    add get location button

--- app/src/main/java/com/example/mapwithquerty/MainActivity.kt
+++ app/src/main/java/com/example/mapwithquerty/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.mapwithquerty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mapwithquerty.ui.theme.MapWithQuertyTheme
 
@@ -26,7 +32,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Button(
+            modifier = Modifier.align(Alignment.Center),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = stringResource(R.string.get_location))
+        }
+    }
 }
 
 @Preview(showBackground = true)
